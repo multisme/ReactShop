dd2ea729a00b56c9c38e64f8417b56f59a1eacdf
multi prise <korokoko.toi@gmail.com>
2021-01-10 12:59:40 +0100

    updated home

--- src/features/home/home.tsx
+++ src/features/home/home.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 
+import ItemsContainer from "features/items/itemsContainer";
+import ShowcaseContainer from "features/showcase/showcaseContainer";
+
 const Home = () => {
   return (
   <div id="home">
+  <ItemsContainer />
+  <ShowcaseContainer />
   </div>
  );
 };
