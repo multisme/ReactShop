72531c6c969eda8935105bb9a79b01e22ecbc31f
multi prise <korokoko.toi@gmail.com>
2021-01-10 12:53:53 +0100

    feature - added items files

---
+++ src/features/items/itemDisplay.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {useDispatch} from "react-redux"
+
+import { itemData } from "features/items/item";
+
+import styles from 'features/items/itemDisplay.module.css';
+
+
+interface Props{
+        items: itemData[]
+}
+
+
+export const ItemListDisplay = ({ items }: Props) => {
+        const renderedlist = items.map((item: itemData) => <ItemDisplay {...item}/>);
+        return <ul className={styles.productList} id="list">{renderedlist}</ul>;
+};
+
+export const ItemDisplay = (item: itemData) => {
+//change the event type here with the correct one
+
+const dispatch = useDispatch();
+
+const selectItem = (e: any) => {
+        e.preventDefault();
+}
+
+  return (
+    <li
+        onClick={selectItem}
+        className={styles.product} key={item.id}
+    >
+      <div className="name">{item.name}</div>
+      <div className={"quantity"}>{item.quantity}</div>
+    </li>
+  );
+};
---
+++ src/features/items/itemsContainer.tsx
@@ -0,0 +1,45 @@
+import React, { Dispatch, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+//import { RootState } from "app/rootReducer";
+
+import { fetchItems, itemsSelector} from "features/items/itemsSlice";
+
+import { ItemListDisplay } from "features/items/itemDisplay";
+
+interface ItemContainerProps {
+  //    page: number
+}
+
+export const ItemsContainer = ({}: //  page = 1
+
+ItemContainerProps) => {
+  const dispatch: Dispatch<any> = useDispatch();
+
+  const { hasError, loading, items } = useSelector(itemsSelector);
+  useEffect(() => {
+            dispatch(fetchItems())
+  }, [dispatch]);
+  console.log(items, hasError, loading);
+
+  if (hasError) {
+    return (
+      <div>
+        <h1>Something went  wrong with the database</h1>
+        <h3>Is the developper payed well enough?</h3>
+      </div>
+    );
+  }
+
+  console.log(items, hasError, loading);
+  console.log(items.length);
+  let renderedContainer = loading ? (
+    <h3>Loading...</h3>
+  ) : (
+    <ItemListDisplay items={items}/>
+  );
+
+  return <ul id="">{renderedContainer}</ul>;
+};
+
+export default ItemsContainer;
