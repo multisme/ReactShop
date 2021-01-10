2d22c031d063e4dc26978e0f55198c1c5a0f95e8
multi prise <korokoko.toi@gmail.com>
2021-01-10 13:09:48 +0100

    archi - move single item logic in it's own folder

--- src/features/item/itemDisplay.tsx
+++ src/features/item/itemDisplay.tsx
@@ -1,21 +1,8 @@
 import React from "react";
 import {useDispatch} from "react-redux"
 
-import { itemData } from "features/items/item";
+import { itemData } from "feature/items/itemSlice";
 import { updateSelectedItem } from "features/showcase/showcaseSlice"
-
-import styles from 'features/items/itemDisplay.module.css';
-
-
-interface Props{
-        items: itemData[]
-}
-
-
-export const ItemListDisplay = ({ items }: Props) => {
-        const renderedlist = items.map((item: itemData) => <ItemDisplay {...item}/>);
-        return <ul className={styles.productList} id="list">{renderedlist}</ul>;
-};
 
 export const ItemDisplay = (item: itemData) => {
 //change the event type here with the correct one
