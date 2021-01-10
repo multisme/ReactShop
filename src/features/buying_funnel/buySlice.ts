c3c5ce234509dcdad67ee6d1e0dde79ed2d2b3ca
multi prise <korokoko.toi@gmail.com>
2021-01-10 14:08:13 +0100

    feature - added buyingfunnelslice template

--- src/features/buying_funnel/buySlice.ts
+++ src/features/buying_funnel/buySlice.ts
@@ -1,1 +1,34 @@
-export {}
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+enum buyingStep{
+        Browsing,
+        Buying,
+        Shipping,
+        Confirmed,
+}
+
+interface buyingFunnelState{
+        step: buyingStep;
+}
+
+const initialState = {
+        step: Browsing
+}
+
+const BuyingFunnelSlice = createSlice({
+        name: "buying_funnel",
+        initialState,
+        reducers: {
+                fromBrownsingToBuying: () => {},
+                fromBuyingToShipping: () => {},
+                fromShippingtoConfirmed: () => {}
+        }
+})
+
+export const{
+        fromBrownsingToBuying,
+        fromBuyingToShipping,
+        fromShippingtoConfirmed
+} = buyingFunnelState.action;
+
+export default BuyingFunnelSlice.reducers
