acc4e3962ff1ed76fca67e6b74a53c33cf4ad0d9
multi prise <korokoko.toi@gmail.com>
2021-01-11 09:26:44 +0100

    tests - tested initial state for itemSlice

--- tests/features/items/itemSlice.test.ts
+++ tests/features/items/itemSlice.test.ts
@@ -1,3 +1,6 @@
+import { Action } from "redux";
+//import fetchMock from 'fetch-mock';
+
 import reducer, {
         fetchItems,
         getItems,
@@ -9,23 +12,49 @@ describe('itemSlice', () => {
         describe('reducers', () => {
                 const initialState = { loading: false, hasError: false, items: [] }
 
+                it('checks if the initalState is correct', () => {
+                        const emptyAction: Action<string> = {type: ""};
+                        const state = reducer(undefined, emptyAction);
+                        expect(state).toEqual(initialState);
+                });
                 it('sets loading true when fetchItems is pending', () => {
                         const state = reducer(initialState, getItems);
                         expect(state).toEqual({ loading: true , hasError: false, items: [] });
                 });
 
                 it('sets the list  when fetchItems is fulfilled', () => {
-                        const action = { type: [fetchItems], payload: { items: [2, 3]} };
+                        const action = { type: [getItemsSuccess], payload: [{id: 1, name: "roro", quantity: 3}] };
                         const state = reducer(initialState, action);
-                        expect(state).toEqual({  loading: false , hasError: false, items: []});
+                        //check if the type is verified somewhere else (the function works with a missing field")
+                        expect(state).toEqual({  loading: false , hasError: false, items: [{id: 1, name: "roro", quantity: 3}]});
                 });
-
 
                 it('sets loading false when fetchItems is rejected', () => {
                         const state = reducer(initialState, getItemsFailure);
                         expect(state).toEqual({  loading: false , hasError: true, items: []});
                 });
-                
+
+        })
+        describe('actions', () => {
+                /*
+                const middlewares = [thunk]
+                const mockStore = configureMockStore(middlewares)
+
+                describe('async actions', () => {
+                        afterEach(() => {
+                                fetchMock.restore()
+                        })
+                        it('fetch items', () =>
+                           fetchMock.getOnce('http://localhost:3000/items', {
+                                   body: [{id: 1, name: "testname", "quantity": 3}]
+                           })
+                           const store = mockStore({ todos: [] })
+                           return store.dispatch(actions.fetchTodos()).then(() => {
+                                   // return of async actions
+                                   expect(store.getActions()).toEqual(expectedActions)
+                           })
+                })
+                */
         })
 })
 
