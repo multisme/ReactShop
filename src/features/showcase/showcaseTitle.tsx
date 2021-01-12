1763f41d33a850c255d6017aea959a28629ea605
multi prise <korokoko.toi@gmail.com>
2021-01-12 14:22:26 +0100

    added missing Showcase Component

---
+++ src/features/showcase/showcaseTitle.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const ShowcaseTitle = () => {
+        return (
+                        <div className={"header showcaseContainer"}>
+                        <section>
+                                <p>THIS IS A SHOPPING WEBSITE</p>
+                                <p>THIS IS A SHOPPING WEBSITE</p>
+                                <p>THIS IS A SHOPPING WEBSITE</p>
+                                <p>THIS IS A SHOPPING WEBSITE</p>
+                                <p>THIS IS A SHOPPING WEBSITE</p>
+                                <p>THIS IS A SHOPPING WEBSITE</p>
+                                <p>THIS IS A SHOPPING WEBSITE</p>
+                                <p>THIS IS A SHOPPING WEBSITE</p>
+                                <p>THIS IS A SHOPPING WEBSITE</p>
+                                <p>THIS IS A SHOPPING WEBSITE</p>
+                                <p>THIS IS A SHOPPING WEBSITE</p>
+                                <p>THIS IS A SHOPPING WEBSITE</p>
+                                <p>THIS IS A SHOPPING WEBSITE</p>
+                                <p>THIS IS A SHOPPING WEBSITE</p>
+                        </section>
+                        </div>
+               )
+}
+
+export default ShowcaseTitle;
