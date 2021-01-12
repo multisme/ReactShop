90e0df49fe8c32f59c0e14201ba73d3065df08f2
multi prise <korokoko.toi@gmail.com>
2021-01-12 17:19:54 +0100

    feature - added cartSlice

---
+++ src/features/cart/cartSlice.ts
@@ -0,0 +1,41 @@
+import React from "react";
+import {
+        createSlice, PayloadAction
+} from "@reduxjs/toolkit";
+
+interface cartItem{
+        id: number,
+        name: string,
+        quantity: number,
+        price: number
+}
+
+interface cartState {
+        selection: cartItem[];
+};
+
+const initialState = {
+        selection: []
+} as cartState
+
+const cartSlice = createSlice({
+        name: "cart",
+        initialState,
+        reducers: {
+                addToCart: (state, { payload }: PayloadAction<cartItem> ) => {
+                        state.selection.push(payload) 
+                },
+                removeFromCart: (state, { payload }: PayloadAction<cartItem>) => {
+                        state.selection = state.selection.filter(
+                                item => item.id != payload.id
+                        )
+                }
+        }
+})
+
+export default cartSlice.reducer;
+
+export const {
+        addToCart,
+        removeFromCart
+} = cartSlice.actions
