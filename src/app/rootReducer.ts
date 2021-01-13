830f28f2a482a2f528f7ab0431901b52ea2ffdaa
multi prise <korokoko.toi@gmail.com>
2021-01-13 01:00:16 +0100

    dev - add cartreducer to rootreducer

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,11 +1,14 @@
 import { combineReducers } from "@reduxjs/toolkit";
+import cartSlice from "features/cart/cartSlice";
 
 import ItemsSliceReducer from "features/items/itemsSlice";
 import showcaseSliceReducer from "features/showcase/showcaseSlice";
+import cartSliceReducer from "features/cart/cartSlice";
 
 const rootReducer = combineReducers({
   items: ItemsSliceReducer,
   showcase: showcaseSliceReducer,
+  cart: cartSliceReducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
