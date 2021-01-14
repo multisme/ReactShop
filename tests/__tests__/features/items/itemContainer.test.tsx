23608a97f734856eb49ba8cced1559f04126e19c
multi prise <korokoko.toi@gmail.com>
2021-01-14 11:11:49 +0100

    test - added test for showcase and corrected tests for itemsContainer

--- tests/__tests__/features/items/itemContainer.test.tsx
+++ tests/__tests__/features/items/itemContainer.test.tsx
@@ -2,85 +2,85 @@ import React from 'react';
 import configureStore from 'redux-mock-store';
 import * as redux from 'react-redux';
 import { Provider } from 'react-redux';
-import thunk, { ThunkDispatch } from "redux-thunk";
-import { mount, shallow } from 'enzyme';
+import { mount } from 'enzyme';
+import { BrowserRouter as Router} from 'react-router-dom';
 
 import { ItemsContainer } from "features/items/itemsContainer";
 import { ItemListDisplay } from "features/items/itemsDisplay";
-import { fetchItems } from "features/items/itemsSlice";
 
-const mockStore = configureStore([thunk]);
+import {
+        getItems,
+        getItemsSuccess,
+        getItemsFailure
+ } from "features/items/itemsSlice";
 
-/*
-jest.mock('features/items/itemsSlice', () => {
-        fetchItems: jest.fn(),
-        itemsSelector: jest.fn()
-});
-*/
+const mockStore = configureStore([]);
 
-
-jest.mock('react-redux', () => {
-        const { Provider } = jest.requireActual("react-redux");
-        return {
-        useSelector: () => ({
-                                hasError: false,
-                                items: [],
-                                loading: false
-       }),
-       useDispatch: jest.fn(()=>{}),
-       Provider
-       }
-});
-
-/*
 describe('itemsContainer', () => {
-        let store: any
-        beforeEach(() => {
-                 // useSelectorMock.mockClear()
-                 // useDispatchMock.mockClear()
-                });
-                const dispatch = jest.fn();
-            //    useDispatch.mockReturnValue(jest.fn());
+        let store: any;
+        const dispatch = jest.fn();
+                  beforeEach(() => {
+                          store = mockStore({
+                  })
+                })
 
                 it("correct component rendered on instanciation", () => {
-                store = mockStore({
-                        items: {
+                        const mock = jest.spyOn(redux, 'useSelector');
+                        mock.mockImplementation(() => ({ 
+                                loading: true,
                                 hasError: false,
-                                items: [],
-                                loading: false
-                     }
-                });
-                        store.dispatch = jest.fn();
+                                items: []
+                        }))
+
+                        const component = mount(
+                                <Provider store={store}>
+                                        <ItemsContainer />
+                                </Provider>
+                        );
+                        const h3 = component.find('h3');
+                        expect(h3.length).toEqual(1);
+                        expect(component).toMatchSnapshot();
+                        expect(h3.text()).toEqual('Loading...')
+                })
+                it("correct component rendered on hasError", () => {
+                        const mock = jest.spyOn(redux, 'useSelector');
+                        mock.mockImplementation(() => ({ 
+                                loading: false,
+                                hasError: true,
+                                items: []
+                        }))
+
                         const component = mount(
                                 <redux.Provider store={store}>
                                         <ItemsContainer />
                                 </redux.Provider>
                         );
-                        expect(store.dispatch).toHaveBeenCalledTimes(1);
-              //          expect(store.dispatch).toHaveBeenCalledWith(
-              //                  fetchItems        
-             //           );
+                        const h1 = component.find('h1');
                         const h3 = component.find('h3');
+                        expect(h1.length).toEqual(1);
                         expect(h3.length).toEqual(1);
-                        expect(h3.text()).toEqual('Loading...')
-                })
-                it("correct component rendered on fetchList error", () => {
-                        const component = mount(
-                                <Provider store={store}>
-                                        <ItemsContainer />
-                                </Provider>
-                        );
                         expect(component).toMatchSnapshot();
+                        expect(h1.text()).toEqual('Something went wrong with the database')
+                        expect(h3.text()).toEqual('Is the developper payed well enough?')
                 })
+                it("correct component rendered", () => {
+                        const mock = jest.spyOn(redux, 'useSelector');
+                        mock.mockImplementation(() => ({ 
+                                loading: false,
+                                hasError: false,
+                                items: [
+                                {id: 3, name: "dede", quantity: 4, price: 5},
+                                {id: 6, name: "dodo", quantity: 2, price: 8}
+                                ]
+                        }))
 
-                it("correct component rendered on fetchList success", () =>
-                {
                         const component = mount(
-                                <Provider store={store}>
+                                <Router>
+                                <redux.Provider store={store}>
                                         <ItemsContainer />
-                                </Provider>
+                                </redux.Provider>
+                                </Router>
                         );
-                        expect(component).toMatchSnapshot();
+                        expect(component.find('.itemCard')).toHaveLength(2);
                 })
 })
-                */
