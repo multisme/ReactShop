7767465849f0dd6f9e5d2a1a79fe7c000b1dbe35
multi prise <korokoko.toi@gmail.com>
2021-01-17 15:02:36 +0100

    added accurate calculations for item prices and quantity

--- src/features/cart/CartPage.tsx
+++ src/features/cart/CartPage.tsx
@@ -1,68 +1,99 @@
-import {useHistory} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
-import {cartItem, cartPageSelector, removeFromCart, updateCartItem} from "features/cart/cartSlice"
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  cartItem,
+  cartPageSelector,
+  removeFromCart,
+  updateCartItem,
+} from "features/cart/cartSlice";
 
-interface cartItemProps{
-        item: cartItem
+interface cartItemProps {
+  item: cartItem;
 }
 
 const CartItem = ({ item }: cartItemProps) => {
+  const [price, setPrice] = useState(0);
+  const [quantity, setQuantity] = useState(item.quantity);
+  const dispatch = useDispatch();
+  const range = [...Array(item.quantity + 1).keys()];
+  const availableQuantities = range.map((i) => (
+    <option value={i} key={i}>
+      {i}
+    </option>
+  ));
 
-                const dispatch = useDispatch();
-                const range = [...Array(item.quantity + 1).keys()]
-                const availableQuantities = range.map((i) => 
-                        <option value={i} key={i}>{i}</option>
-                )
+  const handleQuantity = (event: any) => {
+    event.preventDefault();
+    const new_quantity = event.currentTarget.value;
+    setQuantity(new_quantity);
+    setPrice(item.price * new_quantity)
+  };
 
-        const handleQuantity = (event: any) => {
-                event.preventDefault();
-                dispatch(updateCartItem(item))
-        }
+  const handleClick = (event: any) => {
+    event.preventDefault();
+    dispatch(removeFromCart(item));
+  };
 
-        const handleClick = (event: any) => {
-                event.preventDefault();
-                dispatch(removeFromCart(item))
-        }
-
-        return (
-                <li className={"cartItem"}>
-                        <div className={"picture"}>
-                                <img src={item.url} alt={"item"}/>
-                         </div>
-                        <div className={"name"}>{item.name}</div>
-                        <button className={"remove"} onClick={handleClick}>remove</button>
-                        <select name="quantity" id="quantity" value={item.id} onChange={handleQuantity}>
-                                {availableQuantities}
-                        </select>
-                        <div className={"price"}>{item.price * item.quantity}</div>
-               </li>
-      )
-}
+  return (
+    <li className={"cartItem"}>
+      <div className={"picture"}>
+        <img src={item.url} alt={"item"} />
+      </div>
+      <div className={"cartItemHeader"}>
+        <div className={"name"}>{item.name}</div>
+        <button className={"remove"} onClick={handleClick}>
+          remove
+        </button>
+      </div>
+      <div className={"cartItemBody"}>
+        <div className={"details"}></div>
+        <div className={"priceCalculator"}>
+          <select
+            name="quantity"
+            id="quantity"
+            value={quantity}
+            onChange={handleQuantity}
+          >
+            {availableQuantities}
+          </select>
+          <div className={"price"}>{price}</div>
+        </div>
+      </div>
+    </li>
+  );
+};
 
 const CartPage = () => {
-        const cartItems = useSelector(cartPageSelector);
-        const history = useHistory();
+  const cartItems = useSelector(cartPageSelector);
+  const history = useHistory();
 
-        const handleSubmit = () => {
-                history.push('./bill');
-        }
+  const handleSubmit = () => {
+    history.push("./bill");
+  };
 
-        if (cartItems.length === 0){
-                return (<h3> cart is empty </h3>)
-        }
+  if (cartItems.length === 0) {
+    return <h3> cart is empty </h3>;
+  }
 
-        const rendered_list = cartItems.map((item) =>
-                <CartItem item={item} key={item.id}/>
-        )
-        console.log("cart", cartItems)
-        return (
-                <div className="cartPage">
-               <ul className="cartItems">{rendered_list}</ul>
-                <button type="submit" id="checkout" className="checkout" onSubmit={handleSubmit}>
-                        CHECKOUT
-                </button>
-                </div>
-        )
-}
+  const rendered_list = cartItems.map((item) => (
+    <CartItem item={item} key={item.id} />
+  ));
+  return (
+    <div className="cartPage">
+      <ul className="cartItems">{rendered_list}</ul>
+      <div className="cartTotal">
+      </div>
+      <button
+        type="submit"
+        id="checkout"
+        className="checkout"
+        onSubmit={handleSubmit}
+      >
+        CHECKOUT
+      </button>
+    </div>
+  );
+};
 
-export default CartPage
+export default CartPage;
--- src/features/items/itemPage.tsx
+++ src/features/items/itemPage.tsx
@@ -1,94 +1,86 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {Link} from "react-router-dom";
-import {
-        useHistory,
-        useParams
- } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 
-import {itemPageSelector} from "features/items/itemsSlice";
-import {addToCart} from "features/cart/cartSlice";
+import { itemPageSelector } from "features/items/itemsSlice";
+import { addToCart } from "features/cart/cartSlice";
 
 import { useFormFields } from "utils/utils";
 
-interface productPageParams{
-        id: string
+interface productPageParams {
+  id: string;
 }
 
-const ItemPage = ()=>{
-        const {id}  = useParams<productPageParams>();
-        const [fields, handleFieldChange] = useFormFields({
-                quantity: 0
-        });
-        const dispatch = useDispatch();
-        const [item] = useSelector((state) =>itemPageSelector(state, id));
-        const history = useHistory();
-        if (id === undefined || item === undefined){
-                return (
-                <h3>Error</h3>
-                );
-        }
-       
-        const handleSubmit = (e: any) => {
-                e.preventDefault();
-                if (fields.quantity.content > 0){
-                        dispatch(addToCart({
-                                        id: parseInt(id),
-                                        quantity: parseInt(fields.quantity.content),
-                                        price: item.price,
-                                        url: item.url,
-                                        name: item.name
-                                })
-                        );
-                        history.push("/cart");
-                } else {
-                        alert("Please choose something before checkout");
-                }
-        }
-        
-        const range = [...Array(item.quantity + 1).keys()]
-        const availableQuantities = range.map((i) => 
-                <option value={i} key={i}>{i}</option>
-        )
- 
-        return (
-        <div className="itemPage flex-centered">
-                <div className="panel left flex-centered">
-                <div className="mainImage">
-                <img src={item.url} alt={item.name}/>;
-                </div>
-                </div>
-                <div className="panel right">
-                        <div className="info">
-                                <div className="name">
-                                {item.name}
-                                </div>
-                                <div className="price">
-                                {item.price}€
-                                </div>
-                                <div className="details">
-                                {item.details}
-                                </div>
-                        </div>
-                <form className="itemForm" onSubmit={handleSubmit}>
-                <fieldset className="quantity">
-                <select name="quantity" id="quantity" onChange={handleFieldChange}>
-                        {availableQuantities}
-                </select>
-                </fieldset>
-                <fieldset className="button">
-                <button type="submit" id="checkout" className="checkout">
-                        ADD TO CART
-                </button>
-                <Link to="/">
-                <button id="goBack" className="buy" >
-                        GO BACK
-                </button>
-                </Link>
-                </fieldset>
-                </form>
-                </div>
+const ItemPage = () => {
+  const { id } = useParams<productPageParams>();
+  const [fields, handleFieldChange] = useFormFields({
+    quantity: 0,
+  });
+  const dispatch = useDispatch();
+  const [item] = useSelector((state) => itemPageSelector(state, id));
+  const history = useHistory();
+  if (id === undefined || item === undefined) {
+    return <h3>Error</h3>;
+  }
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    if (fields.quantity.content > 0) {
+      dispatch(
+        addToCart({
+          id: parseInt(id),
+          quantity: parseInt(fields.quantity.content),
+          price: item.price,
+          url: item.url,
+          name: item.name,
+        })
+      );
+      history.push("/cart");
+    } else {
+      alert("Please choose something before checkout");
+    }
+  };
+
+  const range = [...Array(item.quantity + 1).keys()];
+  const availableQuantities = range.map((i) => (
+    <option value={i} key={i}>
+      {i}
+    </option>
+  ));
+
+  return (
+    <div className="itemPage flex-centered">
+      <div className="panel left flex-centered">
+        <div className="mainImage">
+          <img src={item.url} alt={item.name} />;
         </div>
-        )
-}
+      </div>
+      <div className="panel right">
+        <div className="info">
+          <div className="name">{item.name}</div>
+          <div className="price">{item.price}€</div>
+          <div className="details">{item.details}</div>
+        </div>
+        <form className="itemForm" onSubmit={handleSubmit}>
+          <fieldset className="quantity">
+            <select name="quantity" id="quantity" onChange={handleFieldChange}>
+              {availableQuantities}
+            </select>
+          </fieldset>
+          <fieldset className="button">
+            <button type="submit" id="checkout" className="checkout">
+              ADD TO CART
+            </button>
+            <Link to="/">
+              <button id="goBack" className="buy">
+                GO BACK
+              </button>
+            </Link>
+          </fieldset>
+        </form>
+      </div>
+    </div>
+  );
+};
 export default ItemPage;
